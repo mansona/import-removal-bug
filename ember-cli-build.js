02b161508b63b73ca09c0e921e552a780ea4cbe9
Chris Manson <chris@manson.ie>
2024-02-13 11:24:04 +0000

    switch to vite

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,13 +1,22 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const { compatBuild } = require('@embroider/compat');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    'ember-cli-babel': { enableTypeScriptTransform: true },
-
     // Add options here
   });
 
-  return app.toTree();
+  return compatBuild(app, undefined, {
+    staticAddonTrees: true,
+    staticAddonTestSupportTrees: true,
+    staticComponents: true,
+    staticHelpers: true,
+    staticModifiers: true,
+    staticEmberSource: true,
+    amdCompatibility: {
+      es: [],
+    },
+  });
 };
